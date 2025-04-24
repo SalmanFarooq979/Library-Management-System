528c0bc0475b8e1fee52be39975bf88e1e380b0c
SalmanFarooq979 <farooqsalman979@gmail.com>
2025-04-25 01:14:56 +0500

    25-4-2025

--- server/controllers/authcontroller.js
+++ server/controllers/authcontroller.js
@@ -4,4 +4,37 @@ import User from "../models/userModel.js"
 import bycrypt from "bcrypt"
 import crypto from "crypto"
 
-export const register = catchAsyncError(async (req,res,next)=>{})
+export const register = catchAsyncError(async (req,res,next)=>{
+    try {
+        const {name,email,password} = req.body;
+        if(!name || !email || !password){
+            return next(new ErrorHandler("Please enter all fields",400))
+        }
+        const isRegistered = await User.findOne({email, accountVerified: true})
+        if(isRegistered){
+            return next(new ErrorHandler("User already registered",400))
+        }
+        const registrationAttemptsByUser = await User.find({
+            email,
+            accountVerified: false,
+        });
+        if(registrationAttemptsByUser.length >= 5) {
+            return next(new ErrorHandler("You have exceeded the maximum number of registration attempts. Please contact support.", 400));
+        }
+        if(password.length < 8 || password.length > 16){
+            return next(new ErrorHandler("Password must be between 8 and 16 characters",400))
+        }
+        const hashedPassword = await bycrypt.hash(password,10)
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        })
+        const cerificationCode = await user.generateVerificationCode();
+        await user.save();
+        sendVerificationCode(verificationCode,email,res);
+
+    } catch (error) {
+        next(error)
+    }
+})
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,56 +1,71 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    name : {
-        type : String,
-        required : true,
-        trim : true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    email : {
-        type : String,
-        required : true,
-        lowercase: true
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required : true,
-        select : false
+      type: String,
+      required: true,
+      select: false,
     },
-    role : {
-        type : String,
-        enum : ["Admin","User"],
-        default : "User",
+    role: {
+      type: String,
+      enum: ["Admin", "User"],
+      default: "User",
     },
-    accountVarified : {
-        type : Boolean,
-        default : false
+    accountVarified: {
+      type: Boolean,
+      default: false,
     },
     borrowBooks: [
-        {
-            bookId:{
-                type : mongoose.Schema.Types.ObjectId,
-                ref: "Borrow"
-            },
-            returned: {
-                type : Boolean,
-                default : false
-            },
-            bookTitle : String,
-            borrowDate : Date,
-            dueDate : Date,
-        }
+      {
+        bookId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Borrow",
+        },
+        returned: {
+          type: Boolean,
+          default: false,
+        },
+        bookTitle: String,
+        borrowDate: Date,
+        dueDate: Date,
+      },
     ],
     avatar: {
-        public_id : String,
-        url : String
+      public_id: String,
+      url: String,
     },
-    verificationCode : Number,
-    verificationCodeExpired : Date,
-    resetPasswordToken : String,
-    resetPasswordTokenExpired : Date,
-},
-{
+    verificationCode: Number,
+    verificationCodeExpired: Date,
+    resetPasswordToken: String,
+    resetPasswordTokenExpired: Date,
+  },
+  {
     timestamps: true,
-}
+  }
 );
-export const User = mongoose.model("User",userSchema);
+
+userSchema.methods.generateVerificationCode = function () {
+  function generateRandomFiveDigitNumber() {
+    const firstDigit = Math.floor(Math.random() * 9) + 1; // First digit cannot be 0
+    const remainingDigits = Math.floor(Math.random() * 10000)
+      .toString()
+      .padStart(4, 0);
+    return parseInt(firstDigit + remainingDigits); // Generate the remaining 4 digits
+  }
+  const verificationCode = generateRandomFiveDigitNumber();
+  this.verificationCode = verificationCode;
+  this.verificationCodeExpired = Date.now() + 10 * 60 * 1000; // 10 minutes from now
+  return verificationCode;
+};
+export const User = mongoose.model("User", userSchema);
